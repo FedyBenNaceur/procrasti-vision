3971712674c4dc42c19ec8deb3f26e7020d1fd3e
LiliZr <lili@LAPTOP-8H5F1HP1.localdomain>
2023-02-18 11:52:19 +0100

    Customize tooltip

--- procrasti-vis.py
+++ procrasti-vis.py
@@ -63,33 +63,46 @@ def plot_Lili(data):
         units = {'Time': 'Hour(s)', 'Notifications': 'Number', 'Friends': 'Number', 'Groups':'Number'}
 
         # Names of variables we will use
-        x_axis = 'GPA'
+        x_axis = 'Mean'
         y_axis = st.sidebar.selectbox("Y-Axis", measurements, index=measurements.index('Time'))
-
-        # Group by measure given and gender
-        sm_grouped = data.groupby([y_axis, 'Gender']).mean()
-        # Drop gender = 3 (multi index dataframe)
-        sm_grouped = sm_grouped.drop(index=3, level=1).head(15)
-        # Transformes indices to columns
-        source = sm_grouped.reset_index(names=[y_axis, 'Gender'])
-
-        source.replace({'Gender': 1}, 'Men', inplace=True)
-        source.replace({'Gender': 2}, 'Women', inplace=True)
 
         # Gender colors
         dom = ['Men', 'Women']
         rng = ['lightblue', 'hotpink']
+
+        ### Get counts
+        counts = data.groupby([y_axis, 'Gender']).count()
+        # Drop gender = 3 (multi index dataframe)
+        counts = counts.drop(index=3, level=1)
+        # Transformes indices to columns
+        counts = counts.reset_index(names=[y_axis, 'Gender'])
+
+        ### Get other statistics
+        # Group by measure given and gender 
+        statistics = data.groupby([y_axis, 'Gender']).agg(Mean=('GPA', np.mean), 
+                                                 Sum=('GPA', np.sum), 
+                                                 Std=('GPA', np.std),
+                                                 Max=('GPA', np.max),
+                                                 Min=('GPA', np.min))
+        statistics = statistics.drop(index=3, level=1).reset_index(names=[y_axis, 'Gender']).drop(columns=['Gender', y_axis])
+
+        source = pd.concat([counts, statistics], axis=1)
+
+        source.replace({'Gender': 1}, 'Men', inplace=True)
+        source.replace({'Gender': 2}, 'Women', inplace=True)
+
         print(source)
-        # Chart of circles
-        circles = alt.Chart(source).mark_circle(size=200).encode(
-            x=alt.X(x_axis, axis=alt.Axis(orient='top')),
+        ### Chart of circles
+        circles = alt.Chart(source).mark_circle(size=300).encode(
+            x=alt.X(x_axis, title='GPA', axis=alt.Axis(orient='top')),
             y=alt.Y(y_axis, 
                     title = y_axis + '  -  ' + units[y_axis] , 
                     scale=alt.Scale(zero=False, padding=1, domain=[max(source[y_axis]), min(source[y_axis])])),
             color=alt.Color('Gender', scale=alt.Scale(domain=dom, range=rng), legend=alt.Legend(type="symbol"))
+        ).encode(tooltip=['Mean', 'Std', 'Max', 'Min']
         ).interactive()
 
-        # Chart of lines
+        ### Chart of lines
         gpa_mean = source.groupby(by=[y_axis]).mean()
 
 
@@ -98,25 +111,35 @@ def plot_Lili(data):
 
         for index, row in source_men.iterrows():
             if row['Gender'] == 'Women': # Replace values of women by the mean
-                source_men.at[index,'GPA'] = gpa_mean.loc[int(row[y_axis])]['GPA'] 
+                source_men.at[index, x_axis] = gpa_mean.loc[int(row[y_axis])][x_axis] 
             if row['Gender'] == 'Men':  # Replace values of men by the mean
-                source_woman.at[index,'GPA'] = gpa_mean.loc[int(row[y_axis])]['GPA'] 
-
+                source_woman.at[index, x_axis] = gpa_mean.loc[int(row[y_axis])][x_axis] 
+    
+        # Blue lines
         lines_men = alt.Chart(source_men).mark_line().encode(
             x=x_axis,
             y=y_axis,
             detail=y_axis,
             color=alt.value('lightblue')
         )
-
+        # Pink lines
         lines_women = alt.Chart(source_woman).mark_line().encode(
             x=x_axis,
             y=y_axis,
             detail=y_axis,
             color=alt.value('hotpink')
         )
-        # Plot both charts
-        st.altair_chart(circles + lines_men + lines_women, theme="streamlit", use_container_width=True)
+
+        ### Mean GPA
+        gpa = alt.Chart(gpa_mean.reset_index()).mark_circle().encode(
+            x=x_axis,
+            y=y_axis,
+            color=alt.value('purple')
+        ).encode(tooltip=['Mean']
+        ).interactive()
+
+        # Plot charts
+        st.altair_chart(circles + lines_men + lines_women + gpa, theme="streamlit", use_container_width=True)
 
 def aleksandra_plot():
     df = pd.read_csv('SM_Survey_UPSA-2020_clean.csv')
