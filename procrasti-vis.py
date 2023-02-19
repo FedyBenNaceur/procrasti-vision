1d1e69070bc70af7b7d100a6ec139088740be758
Aleksandra Kruchinina <astraljumps@gmail.com>
2023-02-19 21:51:15 +0100

    update intro
    update description

--- procrasti-vis.py
+++ procrasti-vis.py
@@ -19,25 +19,23 @@ def main():
     data = load_data(r'SM_Survey_UPSA-2020.csv')
 
     st.markdown("""
-        ## What is the **correlation/relationship** between social media usage and students’ academic performance ?
+        ## Procrastination through the lenses of social media usage and students’ academic performance.
 
-        This visualisation shows the relationship between students academic performance expressed via `GPA` (grade point average) and 4 related `social media usage metrics`: 
+        Nowadays one strives to go faster and further, be more productive, reach higher goals. But at the same time we observe people delaying tasks, focusing on less urgent and easier ones, being easily distracted. Specially with the accessibility of Internet and medias these new habits may have an addicting effect that could lead to poor health, decreased concentration in class, bad time management, lack of appetite for learning and consequently poor academic performance. So procrastination has become an important issue in our society, and in our project we aimed to study it expressed through the social media usage and its affects on academic studies.
+        
+
+        The **data** was collected from an online survey where a random sample of 623  students from the University of Professional studies, Ghana in 2021.
+
+        The visualisations show the relationship between students academic performance expressed via `GPA` (grade point average) and 4 related `social media usage metrics`: 
         - **Time** refers to average number of hours a student spends daily on social media,
         - **Groups** represents to the number of social media groups a student belongs to,
         - **Freinds** is the number of social media friends a student has,
         - **Notifications** - the average number of times each student checks his phone notifications per day.
-
-        **WHAT** the visualizations show;
-        It is the result of an online survey where a random sample of 623 students was asked to self-reflect about
-        their usage of social media, so Mohammed Nurudeen and his team could measure the effect of social media on
-        academic performance. This application of social media may have some benefits for students’ academic performance.
-        Nonetheless, social media may have an addicting effect that could lead to several things including
-        poor health, poor concentration in class, poor time management, lack of appetite for learning, procrastination
-        and consequently poor academic performance.
+        
 
         **WHO** the visualization is for: parents, teachers and students.
 
-        **WHY** we the audience should care about it: while striving for performance one needs to decrease the potential non productive time.
+        **WHY** the audience should care about it: while striving for performance one needs to decrease the potential non productive time.
 
         """)
   
